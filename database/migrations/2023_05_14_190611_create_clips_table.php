79e6fc16e9fb215d6ca2bb298d53c1e449952b52
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-05-23 12:40:43 +0200

    fix : removed foreignIdFor

--- database/migrations/2023_05_14_190611_create_clips_table.php
+++ database/migrations/2023_05_14_190611_create_clips_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\Author;
 use Domain\Enums\ClipStateEnum;
 
 return new class extends Migration
@@ -15,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('clips', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Author::class);
+            $table->unsignedBigInteger('author_id');
             $table->string('external_id')->unique();
             $table->string('external_game_id');
             $table->string('url');
