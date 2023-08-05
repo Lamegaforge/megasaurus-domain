4d701922586a20461b4698122b76f84271e231b1
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-08-05 20:31:16 +0200

    Updated ClipStateEnum values

--- src/Enums/ClipStateEnum.php
+++ src/Enums/ClipStateEnum.php
@@ -2,13 +2,12 @@
 
 namespace Domain\Enums;
 
-enum ClipStateEnum: string
+enum ClipStateEnum: int
 {
-    case Ok = 'ok';
-    case Alive = 'alive';
-    case Disable = 'disable';
-    case Suspicious = 'suspicious';
-
+    case Ok = 1;
+    case Suspicious = 2;
+    case Disable = 3;
+    
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
