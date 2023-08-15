465b9de5326b0e034067e167a6ff018e67ddabaa
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-08-15 21:47:40 +0200

    oops

--- database/migrations/2023_08_15_214000_add_active_clip_count_on_games_table.php
+++ database/migrations/2023_08_15_214000_add_active_clip_count_on_games_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('clips', function (Blueprint $table) {
+        Schema::table('games', function (Blueprint $table) {
             $table->integer('active_clip_count')->after('duration')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('clips', function (Blueprint $table) {
+        Schema::table('games', function (Blueprint $table) {
             $table->dropColumn('active_clip_count');
         });
     }
